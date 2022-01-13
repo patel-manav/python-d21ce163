d7dc4031b34ea0bce1dad3f3366beadbabc5d437
Manav Patel <76608704+patel-manav@users.noreply.github.com>
2022-01-13 18:34:28 +0530

    Update 2 - Tuple Practical.py

--- Practical 2/2 - Tuple Practical.py
+++ Practical 2/2 - Tuple Practical.py
@@ -1,7 +1,9 @@
 # Tuple Practical - 1
 # Write a Python program to create a tuple with different data types.
 
+# Declare Tuple
 tuple = ("D21CE163",163,True)
+# Print Tuple
 print(tuple)
 
 ################# Created By Manav Dadhaniya - D21CE163 #####################
@@ -9,32 +11,45 @@ print(tuple)
 # Tuple Practical - 2
 # Write a Python program to create a tuple with numbers and print one item.
 
+# Declare Tuple
 tuple = (1,2,3,4,5)
+# Print One Element 
 print(tuple[0])
 
 ################# Created By Manav Dadhaniya - D21CE163 #####################
 
 # Tuple Practical - 3
 # Write a Python program to add an item in a tuple.
+
+# Declare Tuple
 tuple = (1,2,3,4,5)
+# Add & Print Updated Tuple
 print(tuple+(6,))
 
 ################# Created By Manav Dadhaniya - D21CE163 #####################
 
 # Tuple Practical - 4
 # Write a Python program to convert a tuple to a string.
+
+# Delcare Tuple
 tuple = ('D','2','1','C','E','1','6','3')
+# Join All Element Of Tupple In One String Variable
 str = ''.join(tuple)
+# Print String
 print(str)
 
 ################# Created By Manav Dadhaniya - D21CE163 #####################
 
 # Tuple Practical - 5
 # Write a Python program to find the length of a tuple.
+
+# Declare Tupple
 tuple = (10,20,30,40,50)
+# Logic
 sum=0
 for x in tuple:
     sum = sum+1
+# Print Length Of Tupple
 print(sum)
 
 ################# Created By Manav Dadhaniya - D21CE163 #####################
